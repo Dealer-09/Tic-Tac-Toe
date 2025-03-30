d9fed4a851a2983ca048f879391e5c3b7a478c40
Archis <archismanpaul09@outlook.com>
2025-03-30 17:57:07 +0530

    work in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import React  from "react";
+import React from "react";
 import './App.css';
 import Square from "./Square/Square"; 
+
+const renderFrom = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
 const App = () => {
   return (
     <div className="main-div">
-      <div>
+      <div className="move-detection">
+        <div className="left"></div>
+        <div className="right"></div>
+
+      </div>
+      <div className="content-container">
         <h1 className="game-heading water-background">Tic Tac Toe</h1>
         <div className="square-wrapper">
-          <Square/>
+          {renderFrom.flat().map((e, index) => (
+            <Square key={index} />
+          ))}
         </div>
       </div>
     </div>
-
   );
 }
-export default App;
+
+export default App;
